e997ad78a76d295efe144b7763db459386adf612
irving199627 <irving_rosado01@hotmail.com>
2018-07-10 09:44:11 -0500

    first

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,6 +1,5 @@
 import { Component } from '@angular/core';
 import { NavController, LoadingController } from 'ionic-angular';
-import { BusquedaPage } from '../busqueda/busqueda';
 import { FormGroup } from '@angular/forms';
 import { SearchProvider } from '../../providers/search/search';
 
@@ -18,22 +17,7 @@ export class HomePage {
     
   }
   buscar(termino: string){
-    let loading = this.loadingCtrl.create({
-      spinner: 'bubbles',
-      content: 'Buscando, por favor espere'
-    });
-    loading.present();
-      if (this._ss.analisis.length === 0) {
-        this._ss.cargar().then( () => {
-          this._ss.bus(termino);
-          loading.dismiss();
-          this.navCtrl.setRoot(BusquedaPage);
-        }).catch( err => {
-          console.log(err);
-        });
-      } else {
-        this._ss.bus(termino);
-      }
+    
   }
 
 }
--- src/providers/search/search.ts
+++ src/providers/search/search.ts
@@ -19,31 +19,4 @@ export class SearchProvider {
   constructor( private db: AngularFireDatabase ) {
     
   }
-
-  bus( cod: string ) {
-    this.termino = cod.toUpperCase();
-    this.analisis_filtrados = [];
-      this.analisis.forEach( an => {
-        if ( an.estudio.indexOf( this.termino ) >= 0 ) {
-          this.analisis_filtrados.push(an);
-        }
-        console.log(this.analisis_filtrados);
-        if ( this.analisis_filtrados.length === 0 ) {
-          this.vacio = true;
-        } else {
-          this.vacio = false;
-        }
-      });
-  }
-
-  cargar () {
-    const promesa = new Promise( (resolve, reject ) => {
-      this.item = this.db.object('analisis').valueChanges();
-        this.item.forEach( datos => {
-            this.analisis = datos;
-            resolve();
-        });
-    });
-    return promesa;
-  }
 }
